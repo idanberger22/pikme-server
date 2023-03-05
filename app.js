ce6e0db1d3677dec426557be61fa06fba1e8c20b
idanberger22 <idanberger22@gmail.com>
2023-03-05 18:20:56 +0200

    no cors opt

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const corsOptions = {
   origin: ['https://d26z2m6nm787p2.cloudfront.net','https://d26z2m6nm787p2.cloudfront.net/','https://d3d4bwbgnbrnq1.cloudfront.net','https://d3d4bwbgnbrnq1.cloudfront.net/'],
   credentials: true
 }
-app.use(cors(corsOptions))
+app.use(cors())
 
 const generateRTCToken = (req, resp) => {
   const channelName = req.params.channel;
